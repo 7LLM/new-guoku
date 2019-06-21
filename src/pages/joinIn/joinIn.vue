f4e3266a4f9e6c481e7c7c7f77bb1bd6b044c5b0
yingzi <2508743765@qq.com>
2019-06-21 11:24:52 +0800

    添加招商加盟

--- src/pages/joinIn/joinIn.vue
+++ src/pages/joinIn/joinIn.vue
@@ -1,13 +1,91 @@
 <template>
 	<div>
-		joinIn
+		<div class="warp">
+			<div class="joinTop">
+				<div class="row">
+					<br /><br />
+					<h1 style="color: #fff;">果酷-百城合伙人招募计划</h1>
+					<br />
+					<h2 style="color: #fff;">让新鲜和健康便捷可得!</h2>
+				</div>
+			</div>
+			<div class="row">
+				<br /><br />
+				<h1 class="s_font">随着新零售与消费升级，生鲜零售行业多渠道快速发展，销售额突破3万亿。</h1>
+				<br />
+				<h1 class="s_font">您从事生鲜行业优质的合作伙伴，一起逐梦中国生鲜万亿市场!!</h1>
+				<br />
+			    <h1><a href="#" class="s_font">查看申请详情</a></h1>
+			</div>
+			<div class="row">
+				<a href="#"><img src="../../assets/logo/steps.jpg"/></a>
+				<a href="#" class="btn">
+					<p>马上申请</p>
+				</a>
+			</div>
+			<div class="row">
+				<img  style="width: 50%;" src="../../assets/logo/footer-image.jpg"/>
+			</div>
+			<footer>
+				<p>
+					<strong>Copyright © 2010-2018 <a href="http://www.guocool.com">果酷网</a>.</strong>
+					版权所有 京ICP备18056299号
+				</p>
+			</footer>
+		</div>
 	</div>
 </template>
 
 <script>
-	
 </script>
 
-<style>
-	
+<style lang="less">
+	.joinTop {
+		background: url(../../assets/logo/banner_bg.jpg)no-repeat;
+		padding: 10px 0;
+		.warp{
+			width: 1200px;
+			margin:0 auto;
+		}
+	}
+	.row {
+		text-align: center;
+		padding: 20px 0;
+		h1 {
+			font-size: 70px;
+			font-family: "微软雅黑";
+		}
+		h2 {
+			font-size: 42px;
+		}
+		.s_font{
+			font-size: 28px;
+			font-weight:300;
+		}
+		a{
+			color: #3c8dbc;
+		}
+        img{
+        	width: 100%;
+        }
+        .btn{
+        	padding:10px 20px;
+        	display: block;
+        	width:590px;
+        	margin: 0 auto;
+        	background: rgb(251,154,21);
+        	border-radius: 20px;
+        	font-size: 50px;
+        	color: #fff;
+        	font-weight: 400;
+        }
+	}
+	footer{
+		text-align: center;
+		padding: 10px 0 20px 0;
+		margin-bottom: 50px;
+		a{
+			color: #3c8dbc;
+		}
+	}
 </style>
